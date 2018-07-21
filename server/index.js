016090d7626f1847d997a9643b20f4fb2fff42f5
remvst <remvst@gmail.com>
2018-07-21 15:07:55 -0400

    Early sending messages to the client

--- server/index.js
+++ server/index.js
@@ -1,12 +1,28 @@
 'use strict';
 
 const express = require('express');
+const Server = require('http').Server;
+const socketIO = require('socket.io');
+
 const app = express();
+const server = Server(app);
+const io = socketIO.listen(server);
 
 const PORT = parseInt(process.env.PORT) || 5000;
 
 app.use('/', express.static('static'));
 
-app.listen(PORT, () => {
+io.on('connection', client => {
+    setTimeout(() => playMessage('This is a test yolo', client), 1000);
+});
+
+server.listen(PORT, () => {
     console.log('Server started');
 });
+
+function playMessage(messageString, client) {
+    client.emit('play-message', {
+        'message': messageString,
+        'file': '' // TODO
+    });
+}
--- static/js/clock.js
+++ static/js/clock.js
@@ -1,20 +1,49 @@
+function addZeroes(x, n = 2) {
+    x = x.toString();
+    while (x.length < n) {
+        x = '0' + x;
+    }
+    return x;
+}
+
+function updateClocks() {
+    const now = new Date();
+    const s = addZeroes(now.getHours()) + ':' + addZeroes(now.getMinutes());
+
+    document.querySelectorAll('.clock').forEach(clock => {
+        clock.innerHTML = s;
+    });
+}
+
+function showSection(sectionId) {
+    const section = document.querySelector('section#' + sectionId);
+    if (!section) {
+        return;
+    }
+
+    document.querySelectorAll('section').forEach(section => {
+        section.style.display = 'none';
+    });
+
+    section.style.display = 'block';
+}
+
+function showMessage(messageData) {
+    document.querySelector('#message-text').innerHTML = messageData.message;
+
+     // TODO do something with the audio file
+
+     showSection('message');
+
+     setTimeout(() => showSection('clock'), 2000);
+}
+
+// Start
 window.addEventListener('load', () => {
-    function addZeroes(x, n = 2) {
-        x = x.toString();
-        while (x.length < n) {
-            x = '0' + x;
-        }
-        return x;
-    }
+    const socket = io();
+    socket.on('play-message', messageData => showMessage(messageData));
 
-    function updateClocks() {
-        const now = new Date();
-        const s = addZeroes(now.getHours()) + ':' + addZeroes(now.getMinutes());
-
-        document.querySelectorAll('.clock').forEach(clock => {
-            clock.innerHTML = s;
-        });
-    }
-
+    updateClocks();
     setInterval(() => updateClocks(), 1000);
+    showSection('clock');
 });
