83be9a73790155ed5997ba1fb0ebe52b4796ddb4
remvst <remvst@gmail.com>
2018-07-21 19:37:51 -0400

    Early alarm

--- server/index.js
+++ server/index.js
@@ -14,6 +14,14 @@ const io = socketIO.listen(server);
 
 const PORT = parseInt(process.env.PORT) || 5000;
 
+const ALARM_TIMES = [
+    // {'dayOfWeek': 6, 'millisecondsInDay': 10 * 24 * 3600},
+    {'dayOfWeek': 6, 'millisecondsInDay': millisecondsInDay(new Date()) + 10000}
+];
+
+let LAST_ALARM_CHECK = new Date();
+const CLIENTS = [];
+
 app.use('/tmp', express.static('tmp'));
 app.use('/', express.static('static'));
 
@@ -23,10 +31,13 @@ io.on('connection', client => {
     client.on('test-message', () => {
         playMessages(['Test message', 'Hopefully it\'ll work'], client);
     });
+    checkAlarm();
 });
 
 server.listen(PORT, () => {
     console.log('Server started');
+
+    setInterval(checkAlarm, 1000);
 });
 
 function playMessages(messageStrings, client) {
@@ -53,5 +64,45 @@ function playMessages(messageStrings, client) {
                 });
             });
         })
-        .catch(err => console.err(err));
+        .catch(err => console.error(err));
+}
+
+function millisecondsInDay(date) {
+    return (date.getHours() * 3600 + date.getMinutes() * 60 + date.getSeconds()) * 1000;
+}
+
+function ring() {
+    io.sockets.clients((err, clients) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+
+        clients.forEach(socketId => {
+            const client = io.sockets.connected[socketId];
+            playMessages(['Time to ring'], client);
+        });
+    });
+}
+
+function checkAlarm() {
+    const now = new Date();
+
+    let shouldRing = false;
+    ALARM_TIMES
+        .filter(time => time.dayOfWeek === now.getDay())
+        .forEach(settings => {
+            const before = millisecondsInDay(LAST_ALARM_CHECK) - settings.millisecondsInDay > 0;
+            const after = millisecondsInDay(now) - settings.millisecondsInDay > 0;
+            
+            if (before !== after) {
+                shouldRing = true;
+            }
+        });
+
+    if (shouldRing) {
+        ring();
+    }
+
+    LAST_ALARM_CHECK = now;
 }
